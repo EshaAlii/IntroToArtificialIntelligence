118cbdbed74a3aa105f68adaa7ee703c18086f6e
Esha <100985162+EshaAlii@users.noreply.github.com>
2023-11-22 23:05:26 -0800

    Update greedy.py

--- Greedy_A*_Uniform/greedy.py
+++ Greedy_A*_Uniform/greedy.py
@@ -6,39 +6,6 @@
 # used in this function, including the classes RouteProblem, Node, and
 # Frontier. Also, this function uses heuristic function objects defined
 # in the "heuristic.py" file.
-#
-# YOUR COMMENTS INCLUDING CITATIONS
-#
-# Comments:
-# The entirety of this code was created by utilizing prior code and changing up implementation to better fit the
-# Greedy Search pseudocode provided to us in PA1spec.pdf. Essentially what differentiated this code from my prior one
-# was the initial node initialization and the frontier initialization. This specific algorithm is one that was familiar
-# to me given my prior knowledge in CSE 100 with Greedy Algorithms. I did make sure to search up the the Greedy
-# Search algorithm in terms of how it is implemented in AI to ensure that I was not mistaking the algorithm. However,
-# from what I saw, it seemed to fall mostly in line with how I learnt Greedy algorithms functioned.
-#
-#
-# Citations:
-# PA1Spec.pdf -> Greedy Search pseudocode that was provided
-# PA1Spec.pdf -> A* Search pseudocode that was provided
-# PA1Spec.pdf -> Uniform Cost Search pseudocode that was provided
-# PASpec.pdf -> action_cost = road cost = gas amount
-# https://www.geeksforgeeks.org/greedy-best-first-search-algorithm/ -> Review on Greedy Search
-# https://www.geeksforgeeks.org/uniform-cost-search-dijkstra-for-large-graphs/ -> Review on Uniform Cost Search
-# https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/ -> Review on Dijkstra (for ucost)
-# https://blog.finxter.com/python-__delitem__-magic-method/ -> Utilizing front_init.__delitem__(child)
-# https://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html -> Understanding of Heuristic + A* search
-# https://brilliant.org/wiki/a-star-search/#heuristics -> Understanding + Overestimation of Heuristic
-# Conversation with Nick Mount and Andrew Mouillesseaux on Heuristic Function Concept + how to go about implementing it
-#
-#
-# Acknowledgements:
-# Nick Mount -> Helped understand concept behind Heuristic Function
-# Andrew Mouillesseaux -> Helped understand concept behind Heuristic Function + how to go about implementing it
-#
-#
-# Esha Sarfraz - September 29, 2023
-#
 
 from route import Node
 from route import Frontier
